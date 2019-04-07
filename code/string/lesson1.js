427827dfa4898bf3f7c32a0bc6cc46dfe5fe7723
MrZWH <1875290692@qq.com>
2019-04-07 14:54:30 +0800

    反转字符串中的单词

---
+++ code/string/lesson1.js
@@ -0,0 +1,13 @@
+// export default (str) => {
+// 	let arr = str.split(' ')
+// 	let result = arr.map(item => {
+// 		return item.split('').reverse().join('')
+// 	})
+// 	return result.join(' ')
+// }
+
+export default (str) => {
+	return str.split(' ').map(item => {
+		return item.split('').reverse().join('')
+	}).join(' ')
+}
---
+++ test/string/lesson1.test.js
@@ -0,0 +1,5 @@
+import reverByWord from '../../code/string/lesson1'
+
+test('revertByWord:Let\'s take LeetCode contest', () => {
+	expect(revertByWord('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
+})
