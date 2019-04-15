b1ba63ff3213e0aaa36f95132169526c1f9072db
MrZWH <1875290692@qq.com>
2019-04-15 22:05:30 +0800

    栈

---
+++ code/stack/lesson1.js
@@ -0,0 +1,28 @@
+export default (arr) => {
+	let result = []
+	// 上一轮的数据
+	let pre1
+	// 上上一轮的数据
+	let pre2
+	arr.forEach(item => {
+		switch(item) {
+			case 'C':
+				if (result.length) {
+					result.pop()
+				}
+				break
+			case 'D':
+				pre1 = result.pop()
+				result.push(pre1, pre1 * 2)
+				break
+			case '+':
+				pre1 = result.pop()
+				pre2 = result.pop()
+				result.push(pre2, pre1, pre1 + pre2)
+				break
+			default:
+				result.push(item * 1)
+		}
+	})
+	return result.reduce((total, num) => (total + num))
+}
